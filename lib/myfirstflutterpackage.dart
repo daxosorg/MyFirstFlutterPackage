c53024cb6901b15a95efa687cf9fd15f49fdd284
MobileAppDevs <mobile.developer@ongraph.com>
2022-08-14 08:56:01 +0530

    updates

--- lib/myfirstflutterpackage.dart
+++ lib/myfirstflutterpackage.dart
@@ -1,7 +1,26 @@
 library myfirstflutterpackage;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final Widget child;
+  final ButtonStyle? style;
+
+  const CustomButton({Key? key, required this.onPressed, required this.child, this.style}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.all(16.0),
+        primary: Colors.white,
+        backgroundColor: Colors.blue,
+        elevation: 9.0,
+        textStyle: const TextStyle(fontSize: 20),
+      ),
+      child: child,
+    );
+  }
 }
